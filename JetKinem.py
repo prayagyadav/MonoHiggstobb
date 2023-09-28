2f0f59fd4e7f918a0ebb963a5cdb33ed18c0f576
bisnupriyasahu <bisnupriyasahu94@gmail.com>
2023-09-28 05:37:49 -0500

    plots

--- JetKinem.py
+++ JetKinem.py
@@ -5,13 +5,14 @@ import hist
 import matplotlib.pyplot as plt
 import mplhep as hep
 plt.style.use(hep.style.CMS)
+redirector = "root://cmsxrootd.fnal.gov//"
 events = NanoEventsFactory.from_root(
-    "./3BF5BC57-5D24-1C4D-B277-7B0BAA5DF3C7.root",
+    redirector+"/store/mc/RunIIAutumn18NanoAODv7/MonoHTobb_ZpBaryonic_TuneCP2_13TeV_madgraph-pythia8/NANOAODSIM/Nano02Apr2020_rp_102X_upgrade2018_realistic_v21-v1/10000/0EE0D641-EDAE-D547-ABAD-56D54B768C5B.root",
     schemaclass= NanoAODSchema.v7,
     metadata= {"Dataset":"Single Electrons"}
 ).events()
 Jets = events.Jet[events.Jet.pt > 25]
-Tag = hist.Hist.new.Reg(20,-1,1).Double().fill(ak.flatten(Jets.btagCSVV2)).plot()
+Tag = hist.Hist.new.Reg(20,-1,1).Double().fill(ak.flatten(Jets.btagCSVV2))
 Tag.plot()
 plt.savefig("Tag.png", dpi= 320)
 btag_WP_medium = 0.3040
