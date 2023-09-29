4ba1a0c5b1b6190065c4c65d38a95a0d1b85eaad
prayagyadav <prayagyadav206@gmail.com>
2023-09-29 14:35:13 +0530

    Quick Commit

--- JetKinem.py
+++ JetKinem.py
@@ -4,6 +4,7 @@ from coffea.nanoevents import NanoEventsFactory , NanoAODSchema
 import hist
 import matplotlib.pyplot as plt
 import mplhep as hep
+import numpy as np
 plt.style.use(hep.style.CMS)
 redirector = "root://cmsxrootd.fnal.gov//"
 events = NanoEventsFactory.from_root(
@@ -25,4 +26,42 @@ JetHist.plot()
 plt.ylabel("Events/4 GeV")
 hep.cms.label("Preliminary", data=True)
 plt.legend()
-plt.savefig("Jets.png", dpi= 320)
+plt.clf()
+
+#Better Plot
+x_min = 0.
+x_max = 400.
+bin_size = 4
+n_bins=int((x_max - x_min)/bin_size)
+#h = hist.Hist.new.Reg(n_bins,x_min,x_max).Double().fill(ak.flatten(Jets.pt))
+fig , ax= plt.subplots(figsize=(10,10))
+hep.histplot(JetHist["Untagged",:], 
+             #bins=bins ,
+             histtype="fill",
+             color="b",
+             alpha=0.7,
+             edgecolor="black",
+             label=r"Untagged",
+             ax=ax
+            )
+hep.histplot(JetHist["btagDeepFlavB",:], 
+             #bins=bins ,
+             histtype="fill",
+             color="r",
+             alpha=0.7,
+             edgecolor="black",
+             label=r"btagDeepFlavB",
+             ax=ax
+            )
+ax.set_title("Jet $p_t$", fontsize=25, color="#053B50")
+ax.set_xlabel("$p_t$ (GeV)", fontsize=20)
+ax.set_ylabel(f"Events / {bin_size} GeV", fontsize=20)
+ax.set_xticks(np.arange(x_min,x_max+bin_size,bin_size))
+hep.cms.label("Preliminary",data=False, rlabel="unknown $fb^{-1}$")
+
+
+ax.legend()
+fig.savefig("Jets.png", dpi= 300)
+
+
+
