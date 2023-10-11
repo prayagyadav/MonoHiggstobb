82264bd020bd10af792c51450ce9f9a02d169315
prayagyadav <prayagyadav206@gmail.com>
2023-10-11 12:16:51 +0530

    Quick Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""
+This a setup file to initialize the environment before starting the analysis for the first time.
+You do not have to run this file.
+
+Author : Prayag Yadav
+Created : 11 October 2023
+
+"""
+
+#To intialize the package directories
+from setuptools import setup, find_packages
+setup(name='monoHbbtools', version='1.0', packages=find_packages())
