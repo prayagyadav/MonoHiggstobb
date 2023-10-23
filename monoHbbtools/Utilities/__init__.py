23456f8f6bf2f1fc60b38a5f2aeeb2b0143b387b
prayagyadav <prayagyadav@gmail.com>
2023-10-23 18:58:07 +0000

    Automated Commit

--- monoHbbtools/Utilities/__init__.py
+++ monoHbbtools/Utilities/__init__.py
@@ -1,6 +1,19 @@
 import time
 
 def get_timestamp():
+    """
+    Returns the current time according to the local machine.
+    """
     now = time.localtime()
     formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", now)
     return formatted_time
+
+def normalize(Histogram):
+    """
+    Takes in a 1D hist object and normalizes it.
+    """
+    integral = Histogram.sum()
+    
+
+    pass
+
