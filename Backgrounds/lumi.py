b2077efb75a4981904d83e46f7c6501f7320ba8e
prayagyadav <prayagyadav@gmail.com>
2023-10-30 21:48:45 +0530

    Automated Commit

--- Backgrounds/lumi.py
+++ Backgrounds/lumi.py
@@ -1,6 +1,19 @@
-from monoHbbtools.Utilities import lumicalc
+"""
+This file generates a list of luminosity using brilcalc for different runs.
+python 2.7 compatible
+"""
+
+import subprocess
 import json
+
+def Generate_Luminosity_file(runs):
+    runkeys = runs.keys()
+    for run in runkeys :
+        output = subprocess.check_output(["brilcalc","lumi","-c","web","-r", str(run)]).decode("ASCII")
+        out_list = output.split("\n")
+        with open("currentRunLumi.txt","a+") as outfile :    
+            outfile.writelines([line+"\n" for line in out_list])
 
 with open("../CurrentRunFileset.json") as file :
     runs = json.load(file)
-    lumicalc.Generate_Luminosity_file(runs)
+    Generate_Luminosity_file(runs)
