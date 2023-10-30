7746827f4055a32d1a1314b7af97a1aec59e5265
prayagyadav <prayagyadav@gmail.com>
2023-10-30 20:40:36 +0530

    Automated Commit

--- monoHbbtools/Utilities/lumicalc.py
+++ monoHbbtools/Utilities/lumicalc.py
@@ -5,9 +5,8 @@ This file generates a list of luminosity using brilcalc for different runs.
 import os , sys , subprocess
 from coffea import util
 
-def Generate_Luminosity_file(data_coffea):
-    file = util.load(data_coffea)
-    runs = file["MET_Run2018"]["RunSet"] #This is a set of runs
+def Generate_Luminosity_file(data_dict):
+    runs = data_dict["MET_Run2018"]["RunSet"] #This is a set of runs
     luminosities = {}
     for run in runs :
         output = subprocess.check_output(["brilcalc","lumi","-c","web","-r", str(run)]).decode("ASCII")
