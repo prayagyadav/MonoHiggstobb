c5a834eff1c278f82ec2ed9a4f640f10383914b3
prayagyadav <prayagyadav206@gmail.com>
2023-11-21 12:34:33 -0600

    Quick Commit

--- Backgrounds/submit_condor.py
+++ Backgrounds/submit_condor.py
@@ -1,4 +1,3 @@
-from monoHbbtools.Utilities import condor 
 from coffea.nanoevents import NanoAODSchema , NanoEventsFactory
 from coffea import processor
 from coffea.analysis_tools import PackedSelection
@@ -14,6 +13,60 @@ import numpy as np
 #     schemaclass=NanoAODSchema.v7,
 #     metadata={"Dataset":"MET_Run2018"}
 #     ).events()
+
+import os
+
+def move_X509():
+    try:
+        _x509_localpath = (
+            [
+                line
+                for line in os.popen("voms-proxy-info").read().split("\n")
+                if line.startswith("path")
+            ][0]
+            .split(":")[-1]
+            .strip()
+        )
+    except Exception as err:
+        raise RuntimeError(
+            "x509 proxy could not be parsed, try creating it with 'voms-proxy-init'"
+        ) from err
+    _x509_path = f'/scratch/{os.environ["USER"]}/{_x509_localpath.split("/")[-1]}'
+    os.system(f"cp {_x509_localpath} {_x509_path}")
+    return os.path.basename(_x509_localpath)
+
+def runCondor(cores=1, memory="2 GB", disk="1 GB", death_timeout = '60', workers=4):
+    from distributed import Client
+    from dask_jobqueue import HTCondorCluster
+
+    os.environ["CONDOR_CONFIG"] = "/etc/condor/condor_config"
+    _x509_path = move_X509()
+
+    cluster = HTCondorCluster(
+        cores=1,
+        memory="2 GB",
+        disk="1 GB",
+        job_extra_directives={
+            "+JobFlavour": '"longlunch"',
+            "log": "dask_job_output.$(PROCESS).$(CLUSTER).log",
+            "output": "dask_job_output.$(PROCESS).$(CLUSTER).out",
+            "error": "dask_job_output.$(PROCESS).$(CLUSTER).err",
+            "should_transfer_files": "yes",
+            "when_to_transfer_output": "ON_EXIT_OR_EVICT",
+            "+SingularityImage": '"/cvmfs/unpacked.cern.ch/registry.hub.docker.com/coffeateam/coffea-dask-cc7:latest"',
+            "Requirements": "HasSingularityJobStart",
+            "request_GPUs" : "1",
+            "InitialDir": f'/scratch/{os.environ["USER"]}',
+            "transfer_input_files": f'{_x509_path},{os.environ["EXTERNAL_BIND"]}'
+        },
+        job_script_prologue=[
+            "export XRD_RUNFORKHANDLER=1",
+            f"export X509_USER_PROXY={_x509_path}",
+        ]
+    )
+    cluster.adapt(minimum=1, maximum=workers)
+    executor = processor.DaskExecutor(client=Client(cluster))
+    return executor, Client(cluster)
 
 class JetKinem(processor.ProcessorABC):
     def __init__(self):
@@ -77,7 +130,7 @@ elif executor == "condor" :
         level=logging.WARNING,
     )
     print("Preparing to run at condor...\n")
-    executor , client = condor.runCondor()
+    executor , client = runCondor()
 
     runner = processor.Runner(
         executor=executor,
