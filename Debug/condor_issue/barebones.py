d554a4a2c49c44364bcb3e40530e069e06dac6c2
bisnupriyasahu <bisnupriyasahu94@gmail.com>
2023-12-18 03:06:56 -0600

    Quick Commit

--- Debug/condor_issue/barebones.py
+++ Debug/condor_issue/barebones.py
@@ -15,7 +15,7 @@ class barebones(processor.ProcessorABC):
 
 
 events = NanoEventsFactory.from_root(
-    "root://cmsxrd.fnal.gov///store/data/Run2018A/MET/NANOAOD/UL2018_MiniAODv2_NanoAODv9-v2/110000/0F8C0C8C-63E4-1D4E-A8DF-506BDB55BD43.root",
+    "root://cmsxrootd.fnal.gov///store/data/Run2018A/MET/NANOAOD/UL2018_MiniAODv2_NanoAODv9-v2/110000/0F8C0C8C-63E4-1D4E-A8DF-506BDB55BD43.root",
     entry_stop=100000,
     schemaclass=NanoAODSchema,
     metadata={"dataset":"MET_Run2018A"}
