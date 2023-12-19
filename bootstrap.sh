61399c2c1d3dfe600341a2ab4ee3301bcc0f352a
prayagyadav <bisnupriyasahu94@gmail.com>
2023-12-19 07:38:07 -0600

    Quick Commit

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ cat <<EOF > shell
 
 
 if [ "\$1" == "" ]; then
-  export COFFEA_IMAGE=coffeateam/coffea-dask-cc7:latest
+  export COFFEA_IMAGE=coffeateam/coffea-dask:latest-py3.8
 else
   export COFFEA_IMAGE=\$1
 fi
