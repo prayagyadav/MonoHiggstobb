66ee672b7a3fee9895358ed26a47e1fb3dae5d5b
prayagyadav <prayagyadav206@gmail.com>
2024-02-25 09:58:15 -0600

    Quick Commit

--- control/topCR_without_corrections/batch_run/MET_Run2018.sh
+++ control/topCR_without_corrections/batch_run/MET_Run2018.sh
@@ -13,21 +13,21 @@ mkdir -p ./logs
 
 nohup python runner_Top.py -k MET_Run2018 -e condor -c 500000 -cat ${CATEGORY} -lepton ${LEPTON} --begin 1 --end 39 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_MET_Run2018_1_to_39.txt 2>&1 &
 echo "MET_Run2018_1_to_39 submitted with job id $!" | tee -a ./logs/jobids.txt
-sleep 1
+sleep 30
 
 
 nohup python runner_Top.py -k MET_Run2018 -e condor -c 500000 -cat ${CATEGORY} -lepton ${LEPTON} --begin 40 --end 61 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_MET_Run2018_40_to_61.txt 2>&1 &
 echo "MET_Run2018_40_to_61 submitted with job id $!" | tee -a ./logs/jobids.txt
-sleep 1
+sleep 30
 
 
 nohup python runner_Top.py -k MET_Run2018 -e condor -c 500000 -cat ${CATEGORY} -lepton ${LEPTON} --begin 62 --end 85 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_MET_Run2018_62_to_85.txt 2>&1 &
 echo "MET_Run2018_62_to_85 submitted with job id $!" | tee -a ./logs/jobids.txt
-sleep 1
+sleep 30
 
 
 nohup python runner_Top.py -k MET_Run2018 -e condor -c 500000 -cat ${CATEGORY} -lepton ${LEPTON} --begin 86 --end 206 -w ${WORKERS} --redirector ${REDIRECTOR}> ./logs/log_MET_Run2018_86_to_206.txt 2>&1 &
 echo "MET_Run2018_86_to_206 submitted with job id $!" | tee -a ./logs/jobids.txt
-sleep 1
+sleep 30
 
 echo "The End"
